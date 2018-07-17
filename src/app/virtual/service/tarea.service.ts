a68f363f9740b9ec0912ab24a23707ecb28473f5
Sebastián Cajape <jscajape@espe.edu.ec>
2018-07-17 10:21:42 -0500

    Agregada Tarea

--- src/app/virtual/service/tarea.service.ts
+++ src/app/virtual/service/tarea.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { DtoService } from '../../config/global/dto.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
-  constructor() { }
+  constructor(private dto:DtoService) { }
+  
+  obtenerTareasCurso(curso)
+  {
+    return this.dto.ejecutaGet("http://35.197.102.72/aula-virtual-web/webresources/Tarea/"+curso);
+  }
+  guardarTarea(data:any)
+  {
+    return this.dto.ejecutaPut("http://35.197.102.72/aula-virtual-web/webresources/Tarea/",data)
+  }
+  actualizarTarea(data)
+  {
+    console.log("Post");
+    return this.dto.ejecutaPost("http://35.197.102.72/aula-virtual-web/webresources/Tarea/",data);
+  }
+  eliminarTarea(data)
+  {
+    return this.dto.ejecutaDelete2("http://35.197.102.72/aula-virtual-web/webresources/Tarea/"+data);
+  }
 }

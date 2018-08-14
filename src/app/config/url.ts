471b3b097321aa1bc5c30633611066848cc49ddb
EDDanielMarin <yop_dm@hotmail.com>
2018-08-14 15:20:02 -0500

    url

--- src/app/config/url.ts
+++ src/app/config/url.ts
@@ -3,7 +3,7 @@ export var urlUser=
     {modulo:"seguridades",url:"http://130.211.138.144/modulo-seguridades-web/webresources/"},
     {modulo:"espacios",url:"http://13.65.148.23:8084/EspaciosFisicos-web/servicios/"},
     {modulos:"facturacion",url:"http://40.121.33.183:3000/"},
-    {modulo:"periodo",url:"http://35.237.63.186/:80/UniversidadPeriodoLectivo-war/periodoLectivo/"},
+    {modulo:"periodo",url:"http://35.237.63.186:80/UniversidadPeriodoLectivo-war/periodoLectivo/"},
     {modulo:"aula",url:"http://35.197.102.72/aula-virtual-web/webresources/"},
     {modulo:"persona",url:"http://35.229.42.61:3000/personas/"},
     {modulo:"silabus",url:"http://40.117.137.29:3000/"},
